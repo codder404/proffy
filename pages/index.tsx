9a4c388cfb1b4a370f8c6235c80d2f1803a3d8c3
Fernando dos Santos <nando4803@gmail.com>
2020-08-20 23:59:46 +0100

    This commit update all

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,86 @@
 // import Head from 'next/head'
 
-import { Heading } from '@chakra-ui/core';
+import {
+  Grid,
+  Flex,
+  Heading,
+  Input,
+  Link,
+  Button,
+  Text,
+} from '@chakra-ui/core';
 
 export default function Home() {
-  return <Heading size='2xl'>Hello World</Heading>;
+  return (
+    <Grid
+      as='main'
+      height='100vh'
+      templateColumns='1fr 480px 480px 1fr'
+      templateRows='1fr 480px 1fr'
+      templateAreas="
+        '. . . .'
+        '. logo form .'
+        '. . . .'
+      "
+      justifyContent='center'
+      alignItems='center'>
+      <Flex gridArea='logo' flexDir='column' alignItems='flex-start'>
+        <img src='/rocketseat.svg' alt='Rocketseat' />
+
+        <Heading size='2xl' lineHeight='shorter' marginTop={16}>
+          Faça seu login na plataforma
+        </Heading>
+      </Flex>
+      <Flex
+        gridArea='form'
+        height='100%'
+        backgroundColor='gray.700'
+        borderRadius='md'
+        flexDir='column'
+        alignItems='stretch'
+        padding={16}>
+        <Input
+          height='50px'
+          backgroundColor='gray.800'
+          focusBorderColor='purple.500'
+          borderRadius='sm'
+          placeholder='E-mail'
+        />
+        <Input
+          height='50px'
+          backgroundColor='gray.800'
+          focusBorderColor='purple.500'
+          borderRadius='sm'
+          placeholder='Senha'
+          marginTop={2}
+        />
+        <Link
+          alignSelf='flex-start'
+          marginTop={2}
+          fontSize='sm'
+          color='purple.600'
+          fontWeight='bold'
+          _hover={{ color: 'purple.500' }}>
+          Esqueci minha senha
+        </Link>
+        <Button
+          marginTop={6}
+          backgroundColor='purple.500'
+          height='50px'
+          borderRadius='sm'
+          _hover={{ backgroundColor: 'purple.600' }}>
+          ENTRAR
+        </Button>
+        <Text textAlign='center' fontSize='sm' color='gray.300' marginTop={6}>
+          Não tem uma conta?{' '}
+          <Link
+            color='purple.600'
+            fontWeight='bold'
+            _hover={{ color: 'purple.500' }}>
+            Registre-se
+          </Link>
+        </Text>
+      </Flex>
+    </Grid>
+  );
 }
