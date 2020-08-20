7f8641594a791f47f764c1922d1b4202b5ee154a
Fernando dos Santos <nando4803@gmail.com>
2020-08-21 00:20:04 +0100

    This commit import input component

--- components/Input.tsx
+++ components/Input.tsx
@@ -11,6 +11,7 @@ const Input: React.FC<ChakraInputProps> = (props) => {
       backgroundColor='gray.800'
       focusBorderColor='purple.500'
       borderRadius='sm'
+      {...props}
     />
   );
 };
